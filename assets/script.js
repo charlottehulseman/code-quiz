e2b0fda54355c257b335b135adc2e86ab0a7108f
Charlotte Hulseman <chulseman@fordham.edu>
2021-09-29 08:41:14 -0400

    adding high scores file

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+// TODO: Create two arrays for 2-4 quiz questions
+// TODO: Create starter page with rules and start button
+// TODO: Create element to reset global variables when quiz is restarted
+// TODO: Prompt a timer to start when second page appears
+// TODO: Create event listener for when user selects answer
+// TODO: Return correct or incorrect answers
+// TODO: Show final score once user completes quiz
+// TODO: Show all high scores
+
+var quizQuestions = [
+{
+    "questionHeader" : "Which programming language is the most popular for game development?"
+    "one" : "1. ruby",
+    "two" : "2. python",
+    "three" : "3. c++",
+    "four" : "4. rocket science",
+    "correct" : "3. c++",
+},{
+    "quizQuestionHeader" : "Which data type represents the two logic values, true and false?",
+    "one" : "1. number",
+    "two" : "2. string",
+    "three" : "3. 010101",
+    "four" : "4. boolean",
+    "correct" : "4. boolean",
+    },{
+    "quizQuestionHeader" : "Which programming language has the highest level of abstraction?",
+    "one" : "1. python",
+    "two" : "2. fortran",
+    "three" : "3. binary code",
+    "four" : "4. c",
+    "correct" : "1. python",
+    },{
+     "quizQuestionHeader" : "Which is the correct CSS syntax?",
+     "one" : "1. {body;color:black;}",
+     "two" : "2. body {color: black;} ",
+     "three" : "3. body:color=black;",
+     "four" : "4. {body:color=black;}",
+     "correct" : "2. body {color: black;} ",
+    },
+]
